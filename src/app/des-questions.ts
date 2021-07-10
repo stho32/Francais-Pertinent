25a2c438fc0045014ab099c25356e6e31978794c
Stefan Hoffmann <stho32github@gmail.com>
2021-07-10 13:24:04 +0000

    correction miniscule majiscule

--- src/app/des-questions.ts
+++ src/app/des-questions.ts
@@ -9,39 +9,39 @@ export interface Question {
     id: number;
     theme: string;
     question: string;
-    reponses : Reponse[];
-  }
-  
-  export const questions = [
+    reponses: Reponse[];
+}
+
+export const questions = [
     {
         id: 1,
-        theme: "Avoir ou être",
+        theme: "Avoir ou Être",
         question: "Repondre en français: How old are you?",
         reponses: [
-            {id: 1, text: "Je suis ...", correct: false, selectionne:false},
-            {id: 2, text: "Je serai ...", correct: false, selectionne:false},
-            {id: 3, text: "J'ai ...", correct: true, selectionne:false},
-            {id: 4, text: "J'aurai ...", correct: false, selectionne:false}
+            { id: 1, text: "Je suis ...", correct: false, selectionne: false },
+            { id: 2, text: "Je serai ...", correct: false, selectionne: false },
+            { id: 3, text: "J'ai ...", correct: true, selectionne: false },
+            { id: 4, text: "J'aurai ...", correct: false, selectionne: false }
         ]
     },
     {
         id: 2,
-        theme: "Avoir ou être",
+        theme: "Avoir ou Être",
         question: "You have arrived at the destination.",
         reponses: [
-            {id: 1, text: "Vous avez arrivé à destination.", correct: false, selectionne:false},
-            {id: 2, text: "Tu as arrivé à destination.", correct: false, selectionne:false},
-            {id: 3, text: "Vous etes arrivé à destination.", correct: true, selectionne:false},
-            {id: 4, text: "Vous n'avez pas arrivé à destination.", correct: false, selectionne:false}
+            { id: 1, text: "Vous avez arrivé à destination.", correct: false, selectionne: false },
+            { id: 2, text: "Tu as arrivé à destination.", correct: false, selectionne: false },
+            { id: 3, text: "Vous etes arrivé à destination.", correct: true, selectionne: false },
+            { id: 4, text: "Vous n'avez pas arrivé à destination.", correct: false, selectionne: false }
         ]
     },
     {
         id: 3,
-        theme: "Avoir ou être",
+        theme: "Avoir ou Être",
         question: "'I went' in french is ...",
         reponses: [
-            {id: 1, text: "J'ai allé.", correct: false, selectionne:false},
-            {id: 2, text: "Je suis allé.", correct: true, selectionne:false}
+            { id: 1, text: "J'ai allé.", correct: false, selectionne: false },
+            { id: 2, text: "Je suis allé.", correct: true, selectionne: false }
         ]
     }
-  ];
+];
